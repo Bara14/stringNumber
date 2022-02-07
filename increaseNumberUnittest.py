147bcf1c49b6a4bf1cfc31d3a05afe5f4d4aaf47
rkubik <bara14@op.pl>
2022-02-07 13:45:10 +0100

    unittest added

---
+++ increaseNumberUnittest.py
@@ -0,0 +1,27 @@
+import unittest
+
+from main import increaseNumber
+
+
+class TestSum(unittest.TestCase):
+
+    def test_no_digit(self):
+        self.assertEqual(increaseNumber('rtrtrt'), 'rtrtrt1', "Should add 1 ad the end of string")
+
+    def test_one_digit(self):
+        self.assertEqual(increaseNumber('rtrtrt4'), 'rtrtrt5', "Should increase last digit by 1")
+
+    def test_zero_as_last_digit(self):
+        self.assertEqual(increaseNumber('rtrtrt0004000'), 'rtrtrt0004001', "Should increase last digit by 1")
+
+    def test_string_with_digits_not_at_the_end(self):
+        self.assertEqual(increaseNumber('r000trt000rt0004000'), 'r000trt000rt0004001', "Should increase last digit by 1")
+
+    def test_multiple_digit(self):
+        self.assertEqual(increaseNumber('rtrtrt19'), 'rtrtrt20', "Should increase last digits by 1")
+
+    def test_multiple_digit_with_significant_zeros(self):
+        self.assertEqual(increaseNumber('rtrtrt00019'), 'rtrtrt00020', "Should increase last digits by 1 and add significant zeros")
+
+if __name__ == '__main__':
+    unittest.main()
--- main.py
+++ main.py
@@ -9,8 +9,6 @@ def increaseNumber(text):
     if len(substring_with_numbers) != 0:
         reversed_substring_with_numbers = substring_with_numbers[len(substring_with_numbers)::-1]
         increased = str(int(reversed_substring_with_numbers)+1).zfill(len(reversed_substring_with_numbers))
-        print(text.replace(reversed_substring_with_numbers, str(increased)))
+        return text.replace(reversed_substring_with_numbers, str(increased))
     else:
-        print(text +'1')
-
-increaseNumber('foo00001')
+        return text +'1'
