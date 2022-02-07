86204931e514d94206fc1abd3e8fc13f6e2399cc
rkubik <bara14@op.pl>
2022-02-07 10:23:09 +0100

    refactor, adding leading zeros

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 def increaseNumber(text):
-    list_of_numbers =['1','2','3','4','5','6','7','8','9','0']
     index = len(text)
     substring_with_numbers =''
     for _ in reversed(range(0, index)):
-        if text[_] in list_of_numbers:
+        if text[_].isdigit():
             substring_with_numbers = substring_with_numbers + text[_]
         else:
             break
     if len(substring_with_numbers) != 0:
         reversed_substring_with_numbers = substring_with_numbers[len(substring_with_numbers)::-1]
-        to_change = int(reversed_substring_with_numbers) +1
-        print(text.replace(reversed_substring_with_numbers, str(to_change)))
+        numbers_of_zero = reversed_substring_with_numbers[:-1].count('0')
+        if numbers_of_zero <= 1:
+            to_change = str(int(reversed_substring_with_numbers)+1)
+        if numbers_of_zero == 2:
+            to_change = '0'+ str(int(reversed_substring_with_numbers)+1)
+        if numbers_of_zero > 2:
+            to_change = numbers_of_zero*'0' + str(int(reversed_substring_with_numbers)+1)
 
-increaseNumber('aasdfg45667hh003')
+    print(text.replace(reversed_substring_with_numbers, str(to_change)))
+
+increaseNumber('aasdfg45667hh000010')
