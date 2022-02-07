e24630d741f1c3854973b18709a0b41dc443fb1f
rkubik <bara14@op.pl>
2022-02-07 13:18:56 +0100

    refactor, zfill added

--- main.py
+++ main.py
@@ -8,14 +8,7 @@ def increaseNumber(text):
             break
     if len(substring_with_numbers) != 0:
         reversed_substring_with_numbers = substring_with_numbers[len(substring_with_numbers)::-1]
-        numbers_of_zero = reversed_substring_with_numbers[:-1].count('0')
-        if numbers_of_zero <= 1:
-            to_change = str(int(reversed_substring_with_numbers)+1)
-        if numbers_of_zero == 2:
-            to_change = '0'+ str(int(reversed_substring_with_numbers)+1)
-        if numbers_of_zero > 2:
-            to_change = numbers_of_zero*'0' + str(int(reversed_substring_with_numbers)+1)
+        increased = str(int(reversed_substring_with_numbers)+1).zfill(len(reversed_substring_with_numbers))
+    print(text.replace(reversed_substring_with_numbers, str(increased)))
 
-    print(text.replace(reversed_substring_with_numbers, str(to_change)))
-
-increaseNumber('aasdfg45667hh000010')
+increaseNumber('asdf00050001')
