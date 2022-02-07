4c3bcb05c9858fa7dc904f1f78480acdce246ec5
rkubik <bara14@op.pl>
2022-02-07 13:26:32 +0100

    adding excetion when text doesn't contain end-substring with numbers

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def increaseNumber(text):
     if len(substring_with_numbers) != 0:
         reversed_substring_with_numbers = substring_with_numbers[len(substring_with_numbers)::-1]
         increased = str(int(reversed_substring_with_numbers)+1).zfill(len(reversed_substring_with_numbers))
-    print(text.replace(reversed_substring_with_numbers, str(increased)))
+        print(text.replace(reversed_substring_with_numbers, str(increased)))
+    else:
+        print(text +'1')
 
-increaseNumber('asdf00050001')
+increaseNumber('foo00001')
