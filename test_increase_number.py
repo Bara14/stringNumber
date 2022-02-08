5070dc9a4dad49bd06164a970a61fdff9182c303
rkubik <bara14@op.pl>
2022-02-08 08:45:24 +0100

    file nam changed, one test edited

--- test_increase_number.py
+++ test_increase_number.py
@@ -9,7 +9,7 @@ class TestSum(unittest.TestCase):
         self.assertEqual(increaseNumber('rtrtrt'), 'rtrtrt1', "Should add 1 ad the end of string")
 
     def test_one_digit(self):
-        self.assertEqual(increaseNumber('rtrtrt4'), 'rtrtrt5', "Should increase last digit by 1")
+        self.assertEqual(increaseNumber('rtrtrt9'), 'rtrtrt10', "Should increase last digit by 1")
 
     def test_zero_as_last_digit(self):
         self.assertEqual(increaseNumber('rtrtrt0004000'), 'rtrtrt0004001', "Should increase last digit by 1")
@@ -23,5 +23,3 @@ class TestSum(unittest.TestCase):
     def test_multiple_digit_with_significant_zeros(self):
         self.assertEqual(increaseNumber('rtrtrt00019'), 'rtrtrt00020', "Should increase last digits by 1 and add significant zeros")
 
-if __name__ == '__main__':
-    unittest.main()
